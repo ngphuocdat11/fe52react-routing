19ccaf697e2b405ee35442c7c6780fd1a03cc564
ngphuocdat11 <ngphuocdat11@gmail.com>
2020-10-25 16:59:22 +0700

    second commit

---
+++ src/containers/HomeTemplate/ListMoviePage/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+import Movie from './../../../components/Movie';
+
+export default class ListMoviePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listMovie: [],
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: true,
+    });
+    Axios({
+      url:
+        'https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP01',
+      methods: 'GET',
+    })
+      .then((result) => {
+        console.log(result.data);
+        this.setState({
+          listMovie: result.data,
+          loading: false,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  renderHTML = () => {
+    const { listMovie } = this.state;
+    return listMovie.map((movie) => {
+      return <Movie key={movie.maPhim} movie={movie} />;
+    });
+  };
+
+  render() {
+    const { loading } = this.state;
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <h1>ListMoviePage</h1>
+        <div className='row'>{this.renderHTML()}</div>
+      </div>
+    );
+  }
+}
